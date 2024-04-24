1208e6c2127dead2496ed1257392fb02cb0932b0
CodeChef <joetomsim@gmail.com>
2024-04-23 22:06:48 -0500

    added generate markdown and edits

--- index.js
+++ index.js
@@ -1,67 +1,68 @@
-// WHEN I click on the links in the Table of Contents
-// THEN I am taken to the corresponding section of the README
-
-// TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
 
-inquirer.prompt([
-  {
-    type: 'input',
-    message: 'Please enter a title for your project',
-    name: 'title',
-  },
-  {
-    type: 'input',
-    message: 'Please enter a description of the project for your README file',
-    name: 'description',
-  },
-  {
-    type: 'input',
-    message: 'Please enter installation instructions for your application',
-    name: 'installation',
-  },
-  {
-    type: 'input',
-    message: 'Please enter usage information for your application',
-    name: 'usage',
-  },
-  {
-    type: 'input',
-    message: 'Please enter license information',
-    name: 'license',
-    choices: '',
-  },
-  {
-    type: 'input',
-    message: 'Please share any contribution guidlines for your application',
-    name: 'contributing',
-  },
-  {
-    type: 'input',
-    message: 'Please share any testing instructions for your application',
-    name: 'tests',
-  },
-  {
-    type: 'input',
-    message: 'Please enter a link to your Git Hub profile',
-    name: 'gitHub',
-  },
-  {
-    type: 'input',
-    message: 'Please share your email address',
-    name: 'email',
-  },
+function questions() {
+  inquirer.prompt([
+    {
+      type: 'input',
+      message: 'Please enter a title for your project',
+      name: 'title',
+    },
+    {
+      type: 'input',
+      message: 'Please enter a description of the project for your README file',
+      name: 'description',
+    },
+    {
+      type: 'input',
+      message: 'Please enter installation instructions for your application',
+      name: 'installation',
+    },
+    {
+      type: 'input',
+      message: 'Please enter usage information for your application',
+      name: 'usage',
+    },
+    {
+      type: 'list',
+      message: 'Please enter license information',
+      name: 'license',
+      choices: '',
+    },
+    {
+      type: 'input',
+      message: 'Please share any contribution guidlines for your application',
+      name: 'contributing',
+    },
+    {
+      type: 'input',
+      message: 'Please share any testing instructions for your application',
+      name: 'tests',
+    },
+    {
+      type: 'input',
+      message: 'Please enter a link to your Git Hub profile',
+      name: 'gitHub',
+    },
+    {
+      type: 'input',
+      message: 'Please share your email address',
+      name: 'email',
+    },
 
-]).then((response) => {
+  ]).then((data) => {
 
-});
+    fs.writeFile('README.md', generateMarkdown(data), (err) => err
+      ? console.error('An error has occurred')
+      : console.log('Success!')
+    );
+  })
+};
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) { }
-
-// TODO: Create a function to initialize app
-function init() { }
+function init() {
+  questions();
+};
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,3 +1,6 @@
+// WHEN I choose a license for my application from a list of options
+// THEN a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
+
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {}
@@ -10,14 +13,42 @@ function renderLicenseLink(license) {}
 // If there is no license, return an empty string
 function renderLicenseSection(license) {}
 
-// TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+  
+  # ${data.title} ${data.license}
 
+  ## Table of Contents...
+  [Description](#description)
+  [Installation](#installation)
+  [Usage](#usage)
+  [Contributing](#contributing)
+  [License](#license)
+  [Git Hub](#gitHub)
+  [Email](#email)
+
+  ## Description
+  ### ${data.description}
+
+  ## Installation
+  ### ${data.installation}
+
+  ## Usage
+  ### ${data.usage}
+
+  ## Contributing
+  ### ${data.contributing}
+
+  ## License
+  ### ${data.license}
+
+  ## Git Hub
+  ### ${data.gitHub}
+
+  ## E-mail
+  ### ${data.email}
 `;
 }
 
 module.exports = generateMarkdown;
 
-// WHEN I choose a license for my application from a list of options
-// THEN a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
